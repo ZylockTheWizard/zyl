f2c0d6ba1a31fff3eb0796355155184d422cbd78
ZylockTheWizard <cml0011@aol.com>
2025-02-13 23:16:10 -0600

    feat: context menu setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,27 +7,28 @@ import stylistic from '@stylistic/eslint-plugin'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    "settings": {
-      "react": {
-        "version": "detect"
-      }
+    {files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+    {languageOptions: { globals: globals.browser }},
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        "settings": {
+            "react": {
+                "version": "detect"
+            }
+        }
+    },
+    {
+        plugins: {
+            '@stylistic': stylistic
+        }
+    },
+    {
+        rules: {
+            '@stylistic/indent': ['error', 4],
+            '@stylistic/semi': ['error', 'never'],
+            '@stylistic/max-len': ["error", { "code": 100 }]
+        }
     }
-  },
-  {
-    plugins: {
-      '@stylistic': stylistic
-    }
-  },
-  {
-    rules: {
-      '@stylistic/indent': ['error', 2],
-      '@stylistic/semi': ['error', 'never'],
-    }
-  }
 ]
--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+const { ipcRenderer } = window.require('electron')
+
+window.alert = (message: string) => ipcRenderer.send('show-alert', message)
+window.confirm = (message: string) => ipcRenderer.sendSync('show-confirm', message)
+window.addEventListener('contextmenu', (event: MouseEvent) => {
+    ipcRenderer.send('show-context-menu', event.x, event.y)
+})
 
 const root = createRoot(document.body)
 root.render(<h2>Hello from React!</h2>)
