d7935da98cfda5d63bb8f431aba97ef22c249671
ZylockTheWizard <cml0011@aol.com>
2025-03-14 10:30:54 -0500

    feat: move start code

--- src/client/game.tsx
+++ src/client/game.tsx
@@ -1,7 +1,6 @@
 import React, { CSSProperties, useEffect, useRef } from 'react'
 import { Box } from '@mui/material'
-import { GridMaterial } from '@babylonjs/materials'
-import { AbstractMesh, ArcRotateCamera, Engine, MeshBuilder, PointerEventTypes, Scene, StandardMaterial, Texture, Vector3 } from '@babylonjs/core'
+import { start } from './game/start'
 
 export const Game = () => {
     const containerStyles: CSSProperties = {
@@ -22,128 +21,19 @@ export const Game = () => {
 
     const canvasRef = useRef<HTMLCanvasElement>(null)
 
+    const onCanvasContextMenu: React.MouseEventHandler<HTMLCanvasElement> = (event) => {
+        event.preventDefault()
+        event.stopPropagation()
+    }
+
     useEffect(() => {
         const { current: canvas } = canvasRef
-        if (!canvas) return
-
-        const engine = new Engine(canvas, true)
-        const scene = new Scene(engine)
-
-        const camera = new ArcRotateCamera('Camera', -(Math.PI / 2), 0, 30, new Vector3(0, 0, 0), scene)
-        camera.attachControl(canvas, true)
-
-        const rotationState = { x: camera.alpha, y: camera.beta }
-        scene.registerBeforeRender(() => {
-            camera.alpha = rotationState.x
-            camera.beta = rotationState.y
-        })
-        camera.lowerRadiusLimit = 5
-        camera.upperRadiusLimit = 30
-        camera.wheelPrecision = 5
-        camera.inputs.attached.pointers.camera.panningSensibility = 200
-
-        const mapMaterial = new StandardMaterial('map', scene)
-        mapMaterial.emissiveTexture = new Texture(`${window.zylSession.userData.url}/images/map1.png`, scene)
-        const ground = MeshBuilder.CreateGround('map', { width: 33, height: 23.1 }, scene)
-        ground.material = mapMaterial
-        ground.alphaIndex = 0
-
-        const gridMaterial = new GridMaterial('grid', scene)
-        gridMaterial.gridRatio = 23.1 / 21
-        gridMaterial.gridOffset = new Vector3(0, 0, 0.5)
-        gridMaterial.majorUnitFrequency = 0
-        gridMaterial.opacity = 0.99
-        const gridGround = MeshBuilder.CreateGround('grid', { width: 33, height: 23.1 }, scene)
-        gridGround.material = gridMaterial
-        gridGround.position = new Vector3(0, 0.001, 0)
-        gridGround.alphaIndex = 1
-
-        const tokenMaterial = new StandardMaterial('token', scene)
-        const tokenTexture = new Texture(`${window.zylSession.userData.url}/images/token1.png`, scene)
-        tokenMaterial.emissiveTexture = tokenTexture
-        tokenMaterial.opacityTexture = tokenTexture
-        const tokenGround = MeshBuilder.CreateGround('token', { width: 1, height: 1 }, scene)
-        tokenGround.material = tokenMaterial
-        tokenGround.position = new Vector3(0, 0.002, 0)
-        tokenGround.alphaIndex = 2
-
-        let startingPoint: Vector3
-        let currentMesh: AbstractMesh
-
-        const getGroundPosition = function () {
-            const pickinfo = scene.pick(scene.pointerX, scene.pointerY, function (mesh) {
-                return mesh === ground
-            })
-            if (pickinfo.hit) {
-                return pickinfo.pickedPoint
-            }
-
-            return null
-        }
-
-        const pointerDown = function (mesh: AbstractMesh) {
-            currentMesh = mesh
-            startingPoint = getGroundPosition()
-            if (startingPoint) {
-                setTimeout(function () {
-                    camera.detachControl()
-                }, 0)
-            }
-        }
-
-        const pointerUp = function () {
-            if (startingPoint) {
-                camera.attachControl(canvas, true)
-                startingPoint = null
-                return
-            }
-        }
-
-        const pointerMove = function () {
-            if (!startingPoint) {
-                return
-            }
-            const current = getGroundPosition()
-            if (!current) {
-                return
-            }
-
-            const diff = current.subtract(startingPoint)
-            currentMesh.position.addInPlace(diff)
-
-            startingPoint = current
-        }
-
-        scene.onPointerObservable.add((pointerInfo) => {
-            switch (pointerInfo.type) {
-                case PointerEventTypes.POINTERDOWN:
-                    if (pointerInfo.event.button === 0 && pointerInfo.pickInfo.hit && pointerInfo.pickInfo.pickedMesh !== ground) {
-                        pointerDown(pointerInfo.pickInfo.pickedMesh)
-                    }
-                    break
-                case PointerEventTypes.POINTERUP:
-                    pointerUp()
-                    break
-                case PointerEventTypes.POINTERMOVE:
-                    pointerMove()
-                    break
-            }
-        })
-
-        engine.runRenderLoop(() => scene.render())
-        window.addEventListener('resize', () => engine.resize())
+        if (canvas) start(canvas)
     })
 
     return (
         <Box sx={containerStyles}>
-            <canvas
-                style={canvasStyles}
-                ref={canvasRef}
-                onContextMenu={(e) => {
-                    e.preventDefault()
-                    e.stopPropagation()
-                }}
-            />
+            <canvas style={canvasStyles} ref={canvasRef} onContextMenu={onCanvasContextMenu} />
         </Box>
     )
 }
---
+++ src/client/game/start.ts
@@ -0,0 +1,161 @@
+import {
+    AbstractMesh,
+    ArcRotateCamera,
+    Color3,
+    Engine,
+    HighlightLayer,
+    IKeyboardEvent,
+    KeyboardEventTypes,
+    Mesh,
+    MeshBuilder,
+    PointerEventTypes,
+    PointerInfo,
+    Scene,
+    StandardMaterial,
+    Texture,
+    Vector3,
+} from '@babylonjs/core'
+import { GridMaterial } from '@babylonjs/materials'
+
+export const start = (canvas: HTMLCanvasElement) => {
+    const engine = new Engine(canvas, true)
+    const scene = new Scene(engine)
+
+    const camera = new ArcRotateCamera('camera', -(Math.PI / 2), 0, 30, new Vector3(0, 0, 0), scene)
+    camera.attachControl(canvas, true)
+
+    const rotationState = { x: camera.alpha, y: camera.beta }
+    scene.registerBeforeRender(() => {
+        camera.alpha = rotationState.x
+        camera.beta = rotationState.y
+    })
+    camera.lowerRadiusLimit = 5
+    camera.upperRadiusLimit = 30
+    camera.wheelPrecision = 5
+    camera.inputs.attached.pointers.camera.panningSensibility = 200
+
+    const mapMaterial = new StandardMaterial('map-material', scene)
+    mapMaterial.emissiveTexture = new Texture(`${window.zylSession.userData.url}/images/map1.png`, scene)
+    const map = MeshBuilder.CreateGround('map-ground', { width: 33, height: 23.1 }, scene)
+    map.material = mapMaterial
+    map.position = new Vector3(0.05, 0, 0.03)
+    map.alphaIndex = 0
+
+    const gridMaterial = new GridMaterial('grid-material', scene)
+    gridMaterial.gridRatio = 23.1 / 21
+    gridMaterial.gridOffset = new Vector3(0, 0, 0.55)
+    gridMaterial.majorUnitFrequency = 0
+    gridMaterial.opacity = 0.99
+    const gridGround = MeshBuilder.CreateGround('grid-ground', { width: 33, height: 23.1 }, scene)
+    gridGround.material = gridMaterial
+    gridGround.position = new Vector3(0, 0.001, 0)
+    gridGround.alphaIndex = 1
+
+    const tokenMaterial = new StandardMaterial('token-material', scene)
+    const tokenTexture = new Texture(`${window.zylSession.userData.url}/images/token1.png`, scene)
+    tokenMaterial.emissiveTexture = tokenTexture
+    tokenMaterial.opacityTexture = tokenTexture
+    const tokenGround = MeshBuilder.CreateGround('token-ground', { width: gridMaterial.gridRatio, height: gridMaterial.gridRatio }, scene)
+    tokenGround.material = tokenMaterial
+    tokenGround.position = new Vector3(0, 0.002, 0)
+    tokenGround.alphaIndex = 2
+
+    let tokenSelected: AbstractMesh
+    let grabStart: Vector3
+
+    const highlightLayer = new HighlightLayer('highlightLayer', scene, {
+        isStroke: true,
+        mainTextureRatio: 5,
+    })
+
+    const cursorPick = () => scene.pick(scene.pointerX, scene.pointerY)
+
+    const onPointerDown = (pointerInfo: PointerInfo) => {
+        highlightLayer.removeAllMeshes()
+        const pickedMesh = pointerInfo.pickInfo.pickedMesh
+        if (pointerInfo.event.button === 0 && pickedMesh === tokenGround) {
+            tokenSelected = pickedMesh
+            grabStart = cursorPick().pickedPoint
+        } else {
+            tokenSelected = null
+        }
+    }
+
+    const onPointerMove = () => {
+        const hoverMesh = cursorPick()?.pickedMesh
+
+        let cursor = 'default'
+        if (hoverMesh === tokenGround) cursor = 'grab'
+        if (grabStart) cursor = 'grabbing'
+        canvas.style.cursor = cursor
+
+        if (!grabStart) return
+        const current = cursorPick()?.pickedPoint
+        if (!current) return
+        const diff = current.subtract(grabStart)
+        diff.y = 0
+        tokenSelected.position.addInPlace(diff)
+        grabStart = current
+    }
+
+    const onPointerUp = () => {
+        grabStart = null
+        if (tokenSelected) {
+            const pos = tokenSelected.position
+            const gridRatio = gridMaterial.gridRatio
+            const xOffset = gridRatio / 2
+            pos.x = Math.round((pos.x + xOffset) / gridRatio) * gridRatio - xOffset
+            pos.z = Math.round(pos.z / gridRatio) * gridRatio
+            tokenSelected.position = pos
+            highlightLayer.addMesh(tokenSelected as Mesh, Color3.Blue())
+        }
+    }
+
+    scene.onPointerObservable.add((pointerInfo) => {
+        switch (pointerInfo.type) {
+            case PointerEventTypes.POINTERDOWN:
+                onPointerDown(pointerInfo)
+                break
+            case PointerEventTypes.POINTERUP:
+                onPointerUp()
+                break
+            case PointerEventTypes.POINTERMOVE:
+                onPointerMove()
+                break
+        }
+    })
+
+    const onKeyUp = (event: IKeyboardEvent) => {
+        if (tokenSelected) {
+            switch (event.key) {
+                case 'ArrowUp':
+                case 'w':
+                    tokenSelected.position.z += gridMaterial.gridRatio
+                    break
+                case 'ArrowDown':
+                case 's':
+                    tokenSelected.position.z -= gridMaterial.gridRatio
+                    break
+                case 'ArrowRight':
+                case 'd':
+                    tokenSelected.position.x += gridMaterial.gridRatio
+                    break
+                case 'ArrowLeft':
+                case 'a':
+                    tokenSelected.position.x -= gridMaterial.gridRatio
+                    break
+            }
+        }
+    }
+
+    scene.onKeyboardObservable.add((keyboardInfo) => {
+        switch (keyboardInfo.type) {
+            case KeyboardEventTypes.KEYUP:
+                onKeyUp(keyboardInfo.event)
+                break
+        }
+    })
+
+    engine.runRenderLoop(() => scene.render())
+    window.addEventListener('resize', () => engine.resize())
+}
